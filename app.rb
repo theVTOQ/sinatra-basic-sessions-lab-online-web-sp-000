0e77f7f4c5eeb776c7e653efa0cc7945f21f65a8
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-26 21:41:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/checkout' do
+
+  end
+  
 end
