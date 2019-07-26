454c872466c1f3aa63670d102a72fbd873ae840c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-26 21:48:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    @session = session
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+  end
 end
